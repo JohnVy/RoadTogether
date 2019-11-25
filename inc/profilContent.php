20c3eb8f06a10f756a356ed74d30918b4bd374d4
ThomasDubernet <thoms.dubernet@gmail.com>
2019-11-25 16:57:40 +0100

    start profil page

---
+++ inc/profilContent.php
@@ -0,0 +1,66 @@
+<div class="block-back block-back-sm item" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-2">
+                <div class="vcard d-flex mb-6">
+                    <div class="image align-self-center"><img src="img/person_6.jpg" alt="Person here"></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <span class="subheading-sm">Profil</span>
+                <h2 class="heading">Margot S.</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="row mb-5">
+        <div class="col-md-12">
+            <div class="block-search">
+                <form action="post">
+                <!-- row 1 -->
+                    <div class="row">
+                        <div class="col-md-6 mb-3 mb-lg-0 col-lg-3">
+                            <label for="checkin">Du</label>
+                            <div class="field-icon-wrap">
+                                <div class="icon"><span class="far fa-calendar"></span></div>
+                                <input type="text" id="checkin_date" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-3 mb-lg-0 col-lg-3">
+                            <label for="checkin">Au</label>
+                            <div class="field-icon-wrap">
+                                <div class="icon"><span class="far fa-calendar"></span></div>
+                                <input type="text" id="checkout_date" class="form-control">
+                            </div>
+                        </div>
+
+
+                    </div>
+                <!-- row 2 -->
+                    <div class="row">
+
+
+
+
+
+
+                    </div>
+                <!-- row 3 -->
+                    <div class="row">
+
+
+
+
+
+
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
